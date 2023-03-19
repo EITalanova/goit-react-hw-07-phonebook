584c54a9f2f4c0f3a341b16d9be7df711eb8a0ac
EITalanova <elenabelichenko77@gmail.com>
2023-03-19 19:53:56 +0200

    Added filterSlice, functionsContacts

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-import {
-    persistStore,
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
-} from 'redux-persist';
-import storage from "redux-persist/lib/storage";
 import { contactsReducer } from "./contactsSlice";
 
 const persistConfig = {
